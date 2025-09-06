7836b5bad0f377c23293a747daa3f2ff6da7077e
udityogesh <yogeshwars854@gmail.com>
2025-09-06 12:25:06 +0530

    i have make f string

--- Day 2/Tip Calculator Project/solution.py
+++ Day 2/Tip Calculator Project/solution.py
@@ -7,4 +7,4 @@ total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
-print("Each person should pay: ${final_amount}")
+print(f"Each person should pay: ${final_amount}")
